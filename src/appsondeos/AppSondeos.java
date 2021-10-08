bc8469384b848766f4b70d0af0e963d7fb0214fd
Christian <christian.valenzuelamoreno@gmail.com>
2021-10-08 14:52:41 +0200

    Primera versión del proyecto, aun sin eventos.

--- src/appsondeos/AppSondeos.java
+++ src/appsondeos/AppSondeos.java
@@ -5,12 +5,23 @@
  */
 package appsondeos;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -20,23 +31,40 @@ import javafx.stage.Stage;
 public class AppSondeos extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage) throws FileNotFoundException {
+
+        //Crea el menú "Encuesta" y le añade las pestañas "Realizar encuesta" y "Salir"
+        Menu menuEncuesta = new Menu("Encuesta");
+        MenuItem encuesta = new MenuItem("Realizar encuesta");
+        MenuItem salir = new MenuItem("Salir");
+        menuEncuesta.getItems().add(encuesta);
+        menuEncuesta.getItems().add(salir);
+
+        //Crea la barra de menú y le añade el menú "Encuesta"
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().add(menuEncuesta);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        //Crea y añade a la barra de menú el menú "Help"
+        Menu menuHelp = new Menu("Help");
+        menuBar.getMenus().add(menuHelp);
         
-        Scene scene = new Scene(root, 300, 250);
+        //Añadimos la barra de menú a un HBox para añadirle el efecto de barra y juntar los menús
+        HBox contenedorMenu = new HBox(menuBar);
         
-        primaryStage.setTitle("Hello World!");
+        //Añadimos la imagen
+        Image image1 = new Image("menu.jpg");
+        ImageView iv1 = new ImageView();
+        iv1.setImage(image1);
+        
+        //Juntamos el menú y la imagen en un VBox
+        VBox ventana = new VBox();
+        ventana.getChildren().add(contenedorMenu);
+        ventana.getChildren().add(iv1);
+        
+        //Creamos la escena
+        Scene scene = new Scene(ventana, 300, 250);
+        
+        primaryStage.setTitle("");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
