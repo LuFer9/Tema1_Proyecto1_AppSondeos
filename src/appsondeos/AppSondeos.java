d7d5634423fce2cf6e2408d9b8d7beeb23f0a6a6
Christian <christian.valenzuelamoreno@gmail.com>
2021-10-11 09:18:30 +0200

    Mejora de la primera versión del proyecto, añadimos la funcion de abrir otra pestaña al menú "Encuestas", la cual contiene los tabs.

--- src/appsondeos/AppSondeos.java
+++ src/appsondeos/AppSondeos.java
@@ -9,14 +9,19 @@ import java.io.FileNotFoundException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -31,6 +36,18 @@ public class AppSondeos extends Application {
         //Crea el menú "Encuesta" y le añade las pestañas "Realizar encuesta" y "Salir"
         Menu menuEncuesta = new Menu("Encuesta");
         MenuItem encuesta = new MenuItem("Realizar encuesta");
+        
+        //Añadimos la acción de salir al menú "Salir"
+        encuesta.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                Stage myDialog = new Encuestas(primaryStage);
+                myDialog.sizeToScene();
+                myDialog.show();
+            }
+        });   
+        
         MenuItem salir = new MenuItem("Salir");
         
         //Añadimos la acción de salir al menú "Salir"
@@ -57,7 +74,7 @@ public class AppSondeos extends Application {
         HBox contenedorMenu = new HBox(menuBar);
         
         //Añadimos la imagen
-        Image image1 = new Image("menu.jpg");
+        Image image1 = new Image("Images/menu.jpg");
         ImageView iv1 = new ImageView();
         iv1.setImage(image1);
         
@@ -72,8 +89,49 @@ public class AppSondeos extends Application {
         primaryStage.setTitle("");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }   
-
+        
+        
+        
+        }
+        
+    class Encuestas extends Stage {
+        public Encuestas(Stage owner) {
+            super();
+            initOwner(owner);
+            setTitle("Encuestas");
+            initModality(Modality.APPLICATION_MODAL);
+            
+            Group root = new Group();
+            Scene scene = new Scene(root, 250, 150, Color.WHITE);
+            setScene(scene);
+            
+            // Encuesta de deportes
+            Tab tabDeporte = new Tab("Deportes");
+            //tabDeporte.setContent();
+            
+            // Encuesta de animales
+            Tab tabAnimales = new Tab("Animales");
+            //tabAnimales.setContent();
+            
+            // Encuesta de comidas
+            Tab tabComidas = new Tab("Comidas");
+            //tabComidas.setContent();
+            
+            // Encuesta de viajes
+            Tab tabViajes = new Tab("Viajes");
+            //tabViajes.setContent();
+            
+            // Encuesta de lecturas
+            Tab tabLecturas = new Tab("Lecturas");
+            //tabLecturas.setContent();
+            
+            // Añadimos las tabs a la barra de tabs
+            TabPane tabs = new TabPane(tabDeporte,tabAnimales,tabComidas,tabViajes,tabLecturas);
+            
+            root.getChildren().add(tabs);
+        }
+    }
+        
     /**
      * @param args the command line arguments
      */
